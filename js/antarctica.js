0321f91def794495653b7f2666b849988def490d
nakam031 <35510899+nakam031@users.noreply.github.com>
2018-04-30 11:37:37 -0500

    project 2 update

---
+++ js/antarctica.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
+      });
+
+      var message = ["is remote grandeur","is an ice-crowned wonder","an antipodal abode","is a land of fabled creatures"];
+      var image = ["mountain.jpg","ice.jpg","mcmurdo.jpg","penguin.jpg"];
+            var x = Math.floor(Math.random()*image.length);
+            $(".imagemessage").html(message[x]);
+            $(".homeimage").css({"background-image":"url(image/"+image[x]+")"});
+      
+
+      $("#one").click(function() {
+            $("#one").css({"text-decoration": "underline","color":"#000"});
+            $("#two, #three, #four").css({"text-decoration": "none"});
+      });
+
+      $("#two").click(function() {
+            $("#two").css({"text-decoration": "underline","color":"#000"});
+            $("#one, #three, #four").css("text-decoration", "none");
+      });
+
+      $("#three").click(function() {
+            $("#three").css({"text-decoration": "underline","color":"#000"});
+            $("#one, #two, #four").css("text-decoration", "none");
+      });
+
+      $("#four").click(function() {
+            $("#four").css({"text-decoration": "underline","color":"#000"});
+            $("#one, #two, #three").css({"text-decoration": "none"});
+      });
+});
+
+// Scrolling Effect
+
+// $(window).on("scroll", function() {
+//       if($(window).scrollTop()) {
+//             $('nav').addClass('black');
+//       }
+
+//       else {
+//             $('nav').removeClass('black');
+//       }
+// });
+
