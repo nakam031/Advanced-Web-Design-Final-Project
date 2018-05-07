726e67c0237f9009b3d69022d85a28eb2765663d
nakamura031 <2nen.3kumi.26ban@gmail.com>
2018-05-07 17:24:00 -0500

    update

--- js/antarctica.js
+++ js/antarctica.js
@@ -11,36 +11,76 @@ $(document).ready(function() {
       //});
        $("#zero").click(function(){
             $("#home").show();
-            $("#understand, #experience").hide();
+            $("#understand, #experience, #short, #long").hide();
             $("#one, #two, #three, #four").css({"text-decoration": "none"});           
       });     
 
       $("#one").click(function() {
             $("#one").css({"text-decoration": "underline","color":"#000"});
             $("#two, #three, #four").css({"text-decoration": "none"});
-            $(".homeimage, #experience").hide();
+            $(".homeimage, #experience,#short,#long").hide();
             $("#understand").show();
       });
 
       $("#two").click(function() {
             $("#two").css({"text-decoration": "underline","color":"#000"});
             $("#one, #three, #four").css("text-decoration", "none");
-            $(".homeimage, #understand").hide();
+            $(".homeimage, #understand,#short,#long").hide();
             $("#experience").show();
       });
 
       $("#three").click(function() {
             $("#three").css({"text-decoration": "underline","color":"#000"});
             $("#one, #two, #four").css("text-decoration", "none");
+            $(".homeimage, #understand,#experience,#long").hide();
+            $("#short").show();
       });
 
       $("#four").click(function() {
             $("#four").css({"text-decoration": "underline","color":"#000"});
             $("#one, #two, #three").css({"text-decoration": "none"});
+            $(".homeimage, #understand,#experience,#short").hide();
+            $("#long").show();
       });
 
 });
 
+$(document).scroll(function(){
+      var y = $(document).scrollTop();
+      var onetop = $('#exp_one').position().top
+      var onebottom = $("#exp_one").position().top + $("#exp_one").outerHeight(true);
+      var twotop = $('#exp_two').position().top
+      var twobottom = $("#exp_two").position().top + $("#exp_two").outerHeight(true);
+      var threetop = $('#exp_three').position().top
+      var threebottom = $("#exp_three").position().top + $("#exp_three").outerHeight(true);
+      var fourtop = $('#exp_four').position().top
+      var fourbottom = $("#exp_four").position().top + $("#exp_four").outerHeight(true);
+      if (y >=onetop && y<=onebottom){
+            $("#exp_one").css("opacity","1");
+            $(".leftmap").css({"background-image":"url(image/antarctica.svg)"});  
+            $("#exp_two, #exp_three, #exp_four").css("opacity","0.5"); 
+      }
+      
+      else if (y >= twotop && y<=twobottom){
+            $("#exp_two").css("opacity","1");
+            $(".leftmap").css({"background-image":"url(image/antarctica_2.svg)"});
+            $("#exp_one, #exp_three, #exp_four").css("opacity","0.5");
+
+      }
+      else if (y >= threetop && y<=threebottom){
+            $("#exp_three").css("opacity","1");
+            $(".leftmap").css({"background-image":"url(image/antarctica_3.svg)"});
+            $("#exp_one, #exp_two, #exp_four").css("opacity","0.5");
+
+      }
+      else if (y >= fourtop){
+            $("#exp_four").css("opacity","1");
+            $(".leftmap").css({"background-image":"url(image/antarctica_4.svg)"});
+            $("#exp_one, #exp_two, #exp_three").css("opacity","0.5");
+
+      }      
+
+})
 // Scrolling Effect
 
 // $(window).on("scroll", function() {
